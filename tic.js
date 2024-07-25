2a5818685157433f0962e37e46882e0b49ec1a01
rayansatti <rayansatti013@gmail.com>
2024-07-25 23:47:28 +1000

    debugging the gameboard factory

--- tic.js
+++ tic.js
@@ -1,41 +1,31 @@
 //factory function to process the game
- function gameProcess(player, choice){
-    const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    playerXchoices = []
-    playerOchoices = []
-    const winningSenarios = [
-        [0, 3, 6],[6, 3, 0],
-        [1, 4, 7],[7, 4, 1],
-        [2, 5, 8],[8, 5, 2],
-        [0, 1, 2],[2, 1, 0],
-        [3, 4, 5],[5, 4, 3],
-        [6, 7, 8],[8, 7, 6],
-        [0, 4, 8],[8, 4, 0],
-        [1, 4, 6],[6, 4, 1]
-    ]
+ function gameProcess(name, sign, choice){
 
-    function runGame(){
-        if(player === "X"){
-            playerXchoices.push(choice);
-            if(playerXchoices.length >= 3){
-                let choiceSet = new set(playerXchoices);
-                let matches = winningSenarios.filter(value => choiceSet.has(value));
-                console.log(matches);
-            }else{console.log(playerXchoices)}
-            
+    let gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-        }else if(player === "O"){
-            playerOchoices.push(choice);
-            if(playerOchoices.length >= 3){
-                let choiceSet = new set(playerXchoices);
-                let matches = winningSenarios.filter(value => choiceSet.has(value));
-                console.log(matches);
-            }else{console.log(playerOchoices)}
-            
+    return {
+            name: name,
+            sign: sign,
+            GameBoard: function (sign, choice){
+                return{
+                    gameboard.findIndex(choice).sign(sign),
 
-        }
+                    console.log(gameboard);
+                }
+                
+            }
+        }  
     }
-    runGame()
 
-}
-gameProcess()
+   
+
+    
+            let player1 = gameProcess('lee', 'X', 5)
+            let player2 = gameProcess('alis', 'O', 4)
+     console.log(player1);
+      console.log(player2);
+    
+   
+    
+
+
