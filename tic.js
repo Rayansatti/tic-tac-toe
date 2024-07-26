8d6c6685daf655ea52b0a826618011ec02a05a76
rayansatti <rayansatti013@gmail.com>
2024-07-26 21:48:56 +1000

    creates 2 factories first to create players second to adds player's sign on the gameboard

--- tic.js
+++ tic.js
@@ -1,29 +1,40 @@
-//factory function to process the game
- function gameProcess(name, sign, choice){
+//factory function to create players
+ function Player(name, sign, choice){
 
-    let gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
+    
     return {
             name: name,
             sign: sign,
-            GameBoard: function (sign, choice){
-                return{
-                    gameboard.findIndex(choice).sign(sign),
-
-                    console.log(gameboard);
-                }
-                
-            }
+            choice: choice,  
         }  
+
+       
     }
 
-   
-
-    
-            let player1 = gameProcess('lee', 'X', 5)
-            let player2 = gameProcess('alis', 'O', 4)
-     console.log(player1);
+            let player1 = Player('lee', 'X', 5)
+            let player2 = Player('alis', 'O', 4)
+     console.log(typeof player1);
       console.log(player2);
+
+    //another factory thet takes the player 
+    //factory as an argument 
+    //then reterns the new game board with
+    //player's choice
+
+    function GameBoard (Player, sign, choice){
+        let gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let indexOfChoice = gameboard.indexOf(Player.choice);
+        let value = Player.sign;
+        console.log(value);
+        return{
+            newBoard: gameboard.fill(value, indexOfChoice , indexOfChoice + 1)
+        }
+        
+      };
+
+      console.log(GameBoard(player1));
+      console.log(GameBoard(player2));
+   
     
    
     
