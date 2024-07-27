6e6f56f88aafd11e25cbf107f3a632d5288052cb
rayansatti <rayansatti013@gmail.com>
2024-07-27 20:47:40 +1000

    rebuilt the player and gameboard
    adds 2 functions to take players choices
    
    and returns an update of the gameboard

--- tic.js
+++ tic.js
@@ -1,42 +1,66 @@
 //factory function to create players
- function Player(name, sign, choice){
 
-    
+ function Players(firstPlayer, secondPlayer){
+
     return {
-            name: name,
-            sign: sign,
-            choice: choice,  
-        }  
-
-       
+            firstPlayer: firstPlayer,
+            firstSign: 'X',
+            secondPlayer: secondPlayer, 
+            secondSign: 'O' 
+        }   
     }
+    
 
-            let player1 = Player('lee', 'X', 5)
-            let player2 = Player('alis', 'O', 4)
-     console.log(typeof player1);
-      console.log(player2);
 
-    //another factory thet takes the player 
-    //factory as an argument 
-    //then reterns the new game board with
-    //player's choice
-
-    function GameBoard (Player, sign, choice){
-        let gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        let indexOfChoice = gameboard.indexOf(Player.choice);
-        let value = Player.sign;
-        console.log(value);
-        return{
-            newBoard: gameboard.fill(value, indexOfChoice , indexOfChoice + 1)
-        }
+    
+    //game board object 
+    let GameBoard = {
+        gameboard: 
+        [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+    }
         
-      };
 
-      console.log(GameBoard(player1));
-      console.log(GameBoard(player2));
-   
-    
-   
-    
 
+function takeFirstChoice(choice){
+    let firstSign = 'X';
+   // let newBoard;
+    if(choice >= 0 && choice <= 2){
+        if(GameBoard.gameboard[0][choice] === 0){GameBoard.gameboard[0].fill(firstSign, choice, choice + 1);
+        return{newBoard: GameBoard.gameboard}}else{
+            return{alert: 'This block is taken'}}
+    }else if(choice >= 3 && choice <= 5){
+        let choiceEdit = choice - 3;
+        if(GameBoard.gameboard[1][choiceEdit] === 0){GameBoard.gameboard[1].fill(firstSign, choiceEdit, choiceEdit + 1);
+        return{newBoard1: GameBoard.gameboard}}else{
+            return{alert: 'This block is taken'}}
+    }else if(choice >= 6 && choice <= 8){
+        let choiceEdit = choice - 6;
+        if(GameBoard.gameboard[2][choiceEdit] === 0){GameBoard.gameboard[2].fill(firstSign, choiceEdit, choiceEdit + 1);
+        return{newBoard3: GameBoard.gameboard}}else{
+            return{alert: 'This block is taken'}}
+    }
+}
+
+function takeSecondChoice(choice){
+    let firstSign = 'O';
+   // let newBoard;
+    if(choice >= 0 && choice <= 2){
+        if(GameBoard.gameboard[0][choice] === 0){{GameBoard.gameboard[0].fill(firstSign, choice, choice + 1);
+        return{newBoard: GameBoard.gameboard}}}else{
+            return{alert: 'This block is taken'}
+        }
+    }else if(choice >= 3 && choice <= 5){
+        let choiceEdit = choice - 3;
+        if(GameBoard.gameboard[1][choiceEdit] === 0){GameBoard.gameboard[1].fill(firstSign, choiceEdit, choiceEdit + 1);
+        return{newBoard1: GameBoard.gameboard}}else{
+            return{alert: 'This block is taken'}}
+    }else if(choice >= 6 && choice <= 8){
+        let choiceEdit = choice - 6;
+        if(GameBoard.gameboard[2][choiceEdit] === 0){GameBoard.gameboard[2].fill(firstSign, choiceEdit, choiceEdit + 1);
+        return{newBoard3: GameBoard.gameboard}}else{
+            return{alert: 'This block is taken'}}
+    }
+}
 
