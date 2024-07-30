6c6c90a25cb95f49e980c7f1d0a52546c40330c6
rayansatti <rayansatti013@gmail.com>
2024-07-30 21:54:45 +1000

    adds a function checking winning and tie

--- tic.js
+++ tic.js
@@ -37,89 +37,155 @@ function checkBoard(){
 }
 
 function winningSenarios(){
+    let winnerX = [];
+    let winnerO = [];
+
+    function checkDraws(winnerX, winnerO){
+        if(winnerX.length == winnerO.length){
+            return{
+                winner: `Tie`
+            }
+        }else if(winnerX.length > winnerO){
+            return{
+                winner: `${Players.firstPlayer} is the winner`
+            }
+        }else if(winnerX.length < winnerO){
+            return{
+                winner: `${Players.secondPlayer} is the winner`
+            }
+        }
+    }
    
     if(GameBoard.gameboard[0][0] == GameBoard.gameboard[0][1] == GameBoard.gameboard[0][2]){
         if(GameBoard.gameboard[0][0] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[0][0] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
     }
     else if(GameBoard.gameboard[1][0] == GameBoard.gameboard[1][1] == GameBoard.gameboard[1][2]){
         if(GameBoard.gameboard[1][0] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX ,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[1][0] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
     }
     else if(GameBoard.gameboard[2][0] == GameBoard.gameboard[2][1] == GameBoard.gameboard[2][2]){
         if(GameBoard.gameboard[2][0] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX ,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[2][0] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
     else if(GameBoard.gameboard[0][0] == GameBoard.gameboard[1][0] == GameBoard.gameboard[2][0]){
         if(GameBoard.gameboard[0][0] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX ,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[0][0] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
     }
     else if(GameBoard.gameboard[0][1] == GameBoard.gameboard[1][1] == GameBoard.gameboard[2][1]){
         if(GameBoard.gameboard[0][1] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX ,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[0][1] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
     }
     else if(GameBoard.gameboard[0][2] == GameBoard.gameboard[1][2] == GameBoard.gameboard[2][2]){
         if(GameBoard.gameboard[0][2] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX ,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[0][2] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
     }
     else if(GameBoard.gameboard[0][0] == GameBoard.gameboard[1][1] == GameBoard.gameboard[2][2]){if(GameBoard.gameboard[0][0] === 'X'){
         return{
-            winner: `${Players.firstPlayer} is the winner`
+            winner: `${Players.firstPlayer} is the winner`,
+            result: winnerX.push(Players.firstSign),
+            winners: winnerX ,
+            draws: checkDraws()
         }
     }else if(GameBoard.gameboard[0][0] === 'O'){
         return{
-            winner: `${Players.secondPlayer} is the winner`
+            winner: `${Players.secondPlayer} is the winner`,
+            result: winnerO.push(Players.secondSign),
+            winners: winnerO,
+            draws: checkDraws()
         }
     }}
     else if(GameBoard.gameboard[0][2] == GameBoard.gameboard[1][1] == GameBoard.gameboard[2][0]){
         if(GameBoard.gameboard[0][2] === 'X'){
             return{
-                winner: `${Players.firstPlayer} is the winner`
+                winner: `${Players.firstPlayer} is the winner`,
+                result: winnerX.push(Players.firstSign),
+                winners: winnerX ,
+                draws: checkDraws()
             }
         }else if(GameBoard.gameboard[0][2] === 'O'){
             return{
-                winner: `${Players.secondPlayer} is the winner`
+                winner: `${Players.secondPlayer} is the winner`,
+                result: winnerO.push(Players.secondSign),
+                winners: winnerO,
+                draws: checkDraws()
             }
         }
         
@@ -134,8 +200,10 @@ function takeFirstChoice(choice){
     if(choice >= 0 && choice <= 2){
         if(GameBoard.gameboard[0][choice] === 0){
             GameBoard.gameboard[0].fill(firstSign, choice, choice + 1);
+            
         return{newBoard: GameBoard.gameboard}}else if(checkBoard(newBoard) !== 0){
             return{
+                newBoard: GameBoard.gameboard,
                 alert: 'game over',
                 status: winningSenarios(newBoard)
             }
@@ -147,6 +215,7 @@ function takeFirstChoice(choice){
         if(GameBoard.gameboard[1][choiceEdit] === 0){GameBoard.gameboard[1].fill(firstSign, choiceEdit, choiceEdit + 1);
         return{newBoard: GameBoard.gameboard}}else if(checkBoard(newBoard) !== 0){
             return{
+                newBoard: GameBoard.gameboard,
                 alert: 'game over',
                 status: winningSenarios(newBoard)
             }}else if(GameBoard.gameboard[1][choiceEdit] !== 0){
@@ -156,6 +225,7 @@ function takeFirstChoice(choice){
         if(GameBoard.gameboard[2][choiceEdit] === 0){GameBoard.gameboard[2].fill(firstSign, choiceEdit, choiceEdit + 1);
         return{newBoard: GameBoard.gameboard}}else if(checkBoard(newBoard) !== 0){
             return{
+                newBoard: GameBoard.gameboard,
                 alert: 'game over',
                 status: winningSenarios(newBoard)
             }}else if(GameBoard.gameboard[2][choiceEdit] !== 0){
@@ -166,7 +236,7 @@ function takeFirstChoice(choice){
 
 function takeSecondChoice(choice2){
     let firstSign = 'O';
-   // let newBoard;
+    let newBoard;
     if(choice2 >= 0 && choice2 <= 2){
         
     if(GameBoard.gameboard[0][choice2] === 0){
@@ -174,6 +244,7 @@ function takeSecondChoice(choice2){
         return{newBoard: GameBoard.gameboard}}
         }else if(checkBoard(newBoard) !== 0){
             return{
+                newBoard: GameBoard.gameboard,
                 alert: 'game over',
                 status: winningSenarios(newBoard)
             }}else if(GameBoard.gameboard[0][choice2] !== 0){
@@ -185,6 +256,7 @@ function takeSecondChoice(choice2){
             GameBoard.gameboard[1].fill(firstSign, choiceEdit, choiceEdit + 1);
         return{newBoard: GameBoard.gameboard}}else if(checkBoard(newBoard) !== 0){
             return{
+                newBoard: GameBoard.gameboard,
                 alert: 'game over',
                 status: winningSenarios(newBoard)
             }}else if(GameBoard.gameboard[1][choiceEdit] !== 0){
@@ -195,20 +267,22 @@ function takeSecondChoice(choice2){
             GameBoard.gameboard[2].fill(firstSign, choiceEdit, choiceEdit + 1);
         return{newBoard: GameBoard.gameboard}}else if(checkBoard(newBoard) !== 0){
             return{
+                newBoard: GameBoard.gameboard,
                 alert: 'game over',
-                status: winningSenarios(newBoard)
+                status: winningSenarios(GameBoard.gameboard)
             }}else if(GameBoard.gameboard[2][choiceEdit] !== 0){
             return{alert: 'This block is taken'}}
     }
 }
  
+//take players choices in order
 
 
 
 
 
 
-//finish the logic for takefirstchoice and takesecondchoice 
-// to check for game over
-// empliment the logic for checking the winner
+//check for draws
+// create a function to take players choices in order
+// 
 //
